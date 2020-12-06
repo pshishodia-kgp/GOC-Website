ca586f611e84625f6915f0ee55196143d5cd125c
Prashant Shishodia <prashantshishodia.dakshana17@gmail.com>
2020-12-06 19:58:37 +0530

    templating structure and navbar

--- goc/app.py
+++ goc/app.py
@@ -1,30 +1,23 @@
 import os, json
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 
 TEMPLATE_DIR = os.path.join("..", "templates")
 STATIC_DIR = os.path.join("..", "static")
 
-
-# Initialise the app
-def initApp():
-    app = Flask(__name__, template_folder = TEMPLATE_DIR, static_folder = STATIC_DIR)
-    app.debug = True
-    return app
-
+app = Flask(__name__, template_folder = TEMPLATE_DIR, static_folder = STATIC_DIR)
 
 # Home Page
 @app.route('/')
 def home():
-    return "Welcome to Grimoire of Code homepage. WIP"
+    return render_template('home.j2', title = '')
 
 
 # Listing the blogs
 @app.route('/blogs')
 def blogList():
-    pass
+    return render_template('allblogs.j2', title = 'Blogs')
 
 
 if __name__ == '__main__':
-
-    app = initApp()
+    app.debug = True
     app.run("0.0.0.0", port = 8000)
