bd5d7eef784deda592ac9acb3dd537da6b0db494
Prashant Shishodia <33901783+pshishod2645@users.noreply.github.com>
2020-12-07 08:13:34 +0530

    Blogs frontend (#10)
    * templating structure and navbar
    
    * added footer
    
    * all blogs page updated
    
    * added structure for individual blogs

--- goc/app.py
+++ goc/app.py
@@ -1,30 +1,61 @@
 import os, json
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for, request
 
 TEMPLATE_DIR = os.path.join("..", "templates")
 STATIC_DIR = os.path.join("..", "static")
 
-
-# Initialise the app
-def initApp():
-    app = Flask(__name__, template_folder = TEMPLATE_DIR, static_folder = STATIC_DIR)
-    app.debug = True
-    return app
-
+app = Flask(__name__, template_folder = TEMPLATE_DIR, static_folder = STATIC_DIR)
 
 # Home Page
 @app.route('/')
 def home():
-    return "Welcome to Grimoire of Code homepage. WIP"
+    return render_template('home.j2', title = '')
 
 
 # Listing the blogs
 @app.route('/blogs')
 def blogList():
+    # tags should always include all distinct company name (do it while inserting in database)
+    # Published at should store time gap 
+    # Only need to send these columns. 
+    blogs = [{'id': '3434', 'title' : 'First blog', 
+        'content' :  'hello my name is blah blah blah, welcome to blah blah blah', 
+        'published_at': '2 days ago', 'tags': ['google', 'facebook', 'help', 'hello', 'bye', 'hehe', 'wtf', 'last'],
+        'author': 'thelethalcode'}, 
+        {'id': '3434', 'title' : 'Second Blog', 
+        'content' :  'hello my name is blah blah blah, welcome to blah blah blah', 
+        'published_at': '2 days ago', 'tags': ['google', 'facebook', 'help', 'hello', 'bye', 'hehe'],
+        'author': 'fugazi'}]
+    
+    allTags = ['google', 'facebook', 'help', 'hello', 'wtf', 'blah', 'fugazi', 'lethalcode']
+    return render_template('allblogs.j2', title = 'Blogs', blogs = blogs, allTags = allTags)
+
+@app.route('/blog')           ## get single blog having given id
+def blog():
+    blog_id = request.args.get('blog_id') 
+    blog = {
+        'id': '3434', 'title' : 'Second Blog',
+        'content' :  'hello my name is blah blah blah, welcome to blah blah blah',
+        'shortlisting_content' : 'shortlisting rounds were easy aF',
+        'shortlisting_rounds' : [{'company_name' : 'google', 'content': 'Idk it was usual'}, {'company_name' : 'uber', 'content': 'Idk it was usual'}],
+        'interview_content' : 'yeah, the usual stuff but they ask a shitload of crap too',
+        'interview_rounds': [{'company_name' : 'facebook', 'content': 'Idk it was usual'}, {'company_name' : 'nutanix', 'content': 'Idk it was usual'}],
+        'published_at': '2 days ago', 'tags': ['google', 'facebook', 'help', 'hello', 'bye', 'hehe', 'wtf', 'last'],
+        'author': 'thelethalcode'
+    }
+    if(blog_id == '3434'):
+        return render_template('blog.j2', title = blog['title'], blog = blog)
+    else : 
+        return 'Error'
+
+@app.route('/login')
+def login(): 
     pass
 
+@app.route('/signup')
+def signup(): 
+    pass
 
 if __name__ == '__main__':
-
-    app = initApp()
+    app.debug = True
     app.run("0.0.0.0", port = 8000)
