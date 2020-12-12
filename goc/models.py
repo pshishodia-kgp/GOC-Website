7329ab3da8a57c7c7b352bebc1cee708ec9942ba
tsaraf20 <saraft20@gmail.com>
2020-12-12 18:09:34 +0530

    Adds column for user to database

--- goc/models.py
+++ goc/models.py
@@ -45,3 +45,12 @@ class Round(db.Model):
     company_name = db.Column(db.String(20), nullable=False)
     content = db.Column(db.Text, nullable=False)
     blog = db.Column(db.Integer, db.ForeignKey('blog.id'), nullable=True)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(60), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', {'self.image_file'})"
