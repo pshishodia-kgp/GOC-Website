5b4ce7a2fe8d3e04a740629653b06fcaff58dc52
anuratb <51503937+anuratb@users.noreply.github.com>
2020-12-15 00:44:35 +0530

    Update forms.py

--- goc/forms.py
+++ goc/forms.py
@@ -1,29 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField,IntegerField, FieldList,FormField
-from wtforms.fields.html5 import DateTimeLocalField
+
 from wtforms.validators import DataRequired, Length
 
-class ShortlistingRound:
-    company_name = StringField('Company Name',
-                                 validators = [DataRequired(),Length(min=2)])
-    content = StringField('Content',
-                                 validators = [DataRequired(),Length(min=2)])
-class InterveiwRound:
-    company_name = StringField('Company Name',
-                                 validators = [DataRequired(),Length(min=2)])
-    content = StringField('Content',
-                                 validators = [DataRequired(),Length(min=2)])
+class ShortlistingRound(FlaskForm):
+    company_name = StringField('Company Name')
+    content = StringField('Content')
+class InterveiwRound(FlaskForm):
+    company_name = StringField('Company Name')
+    content = StringField('Content')
 class BlogForm(FlaskForm):
-    id = IntegerField('BlogID',
+    id = StringField('BlogID',
                         validators = [DataRequired()])
+    title = StringField('Title', validators = [DataRequired()]) 
     content = StringField('Content',
                             validators = [DataRequired()])
     shortlisting_content = StringField('ShortlistingContent')
-    shortlisting_rounds = FieldList(FormField(ShortlistingRound),min_entries = 1 )
-    interveiw_content = StringField('InterveiwContent')
-    interveiw_rounds = FieldList(FormField(InterveiwRound),min_entries = 1 )
-    published_at = DateTimeLocalField('Published At',format = '%d/%m/%y',
-                                      validators = [DataRequired()])
+    shortlisting_rounds = FieldList(FormField(ShortlistingRound) )
+    interview_content = StringField('InterveiwContent')
+    interview_rounds = FieldList(FormField(InterveiwRound) )    
     tags = FieldList(StringField('Tag',validators = [DataRequired()]),min_entries = 1)
-    author = StringField('Author' , validators = [DataRequired(),Length(min=2)])    
+    author = StringField('Author' , validators = [DataRequired(),Length(min=2)]) 
+    addTag = SubmitField('Add Another Tag')   
+    addShortListing = SubmitField('Add Company for shortlisting rounds')
+    addInterview = SubmitField('Add Company for interview rounds')
     submit = SubmitField('Add Blog')
