5fc58c4638a1af7a6ea09e3f7253e22d6f858f92
anuratb <51503937+anuratb@users.noreply.github.com>
2020-12-15 20:37:40 +0530

    Update forms.py

--- goc/forms.py
+++ goc/forms.py
@@ -1,27 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField,IntegerField, FieldList,FormField
-
-from wtforms.validators import DataRequired, Length
+from wtforms.widgets import TextArea
+from wtforms.validators import DataRequired, Length,ValidationError
 
 class ShortlistingRound(FlaskForm):
-    company_name = StringField('Company Name')
-    content = StringField('Content')
-class InterveiwRound(FlaskForm):
-    company_name = StringField('Company Name')
-    content = StringField('Content')
-class BlogForm(FlaskForm):
-    id = StringField('BlogID',
-                        validators = [DataRequired()])
+    company_name = StringField('Company Name',validators = [DataRequired()])
+    content = StringField('Content',validators = [DataRequired()],widget = TextArea())
+class InterviewRound(FlaskForm):
+    company_name = StringField('Company Name',validators = [DataRequired()])
+    content = StringField('Content',validators = [DataRequired()],widget = TextArea())
+class Shortlisting(FlaskForm):
+    shortlisting_content = StringField('ShortlistingContent',validators = [DataRequired()],widget = TextArea())
+    shortlisting_rounds = FieldList(FormField(ShortlistingRound) ,min_entries=1)
+class Interview(FlaskForm):
+    interview_content = StringField('InterviewContent',widget = TextArea())
+    interview_rounds = FieldList(FormField(InterviewRound) )  
+class BlogForm(FlaskForm):        
     title = StringField('Title', validators = [DataRequired()]) 
     content = StringField('Content',
-                            validators = [DataRequired()])
-    shortlisting_content = StringField('ShortlistingContent')
-    shortlisting_rounds = FieldList(FormField(ShortlistingRound) )
-    interview_content = StringField('InterveiwContent')
-    interview_rounds = FieldList(FormField(InterveiwRound) )    
+                            validators = [DataRequired()],widget = TextArea())    
+    shortlisting = FormField(Shortlisting)
+    interview = FormField(Interview)      
     tags = FieldList(StringField('Tag',validators = [DataRequired()]),min_entries = 1)
     author = StringField('Author' , validators = [DataRequired(),Length(min=2)]) 
     addTag = SubmitField('Add Another Tag')   
     addShortListing = SubmitField('Add Company for shortlisting rounds')
     addInterview = SubmitField('Add Company for interview rounds')
+    isSelected = SubmitField('Were You Shortlisted')
     submit = SubmitField('Add Blog')
+    
+
