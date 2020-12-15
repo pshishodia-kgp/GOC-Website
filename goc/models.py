45523bc95cd162ffd08b9bd7ec797333186ec91b
anuratb <51503937+anuratb@users.noreply.github.com>
2020-12-15 20:38:25 +0530

    Update models.py

--- goc/models.py
+++ goc/models.py
@@ -5,21 +5,25 @@ import enum
 db = SQLAlchemy()
 
 
-class Blog(db.Model):
+
+class Blog(db.Model):    
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
     published_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
-    tags = db.relationship('Tag', backref='blog', lazy=True)
-    rounds = db.relationship('Round', backref='blog', lazy=True)
-    author = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    tags = db.relationship('Tag',backref='blogg',lazy = True)
+    shortlisting_content = db.Column(db.Text,nullable = False)
+    interview_content = db.Column(db.Text,nullable = True)
+    rounds = db.relationship('Round', backref='blogg', lazy=True)
+    author = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)    
 
     def __repr__(self):
         return self.title
 
 
-class Tag(db.Model):
-    name = db.Column(db.String(20), nullable=False, primary_key=True)
+class Tag(db.Model):     
+    id = db.Column(db.Integer,primary_key = True,nullable = False)
+    name = db.Column(db.String(20),nullable=False)    
     blog = db.Column(db.Integer, db.ForeignKey('blog.id'), nullable=True)
 
     def __repr__(self):
@@ -29,7 +33,7 @@ class Tag(db.Model):
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
-    blogs = db.relationship('Blog', backref='author', lazy=True)
+    blogs = db.relationship('Blog', backref='authorr', lazy=True)
 
     def __repr__(self):
         return self.name
