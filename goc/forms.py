7d17873ea0912b4d6b6203d29c3de9ad7a829acd
tsaraf20 <saraft20@gmail.com>
2020-12-18 13:23:26 +0530

    Removes author class and integrates with User class. Validates username or email while Logging in

--- goc/forms.py
+++ goc/forms.py
@@ -5,7 +5,7 @@ from goc.models import User
 
 class SignUpForm(FlaskForm):
     username = StringField('Username (Codeforces)', validators=[DataRequired(), Length(max=60)])
-    email = StringField('Gmail', validators=[DataRequired(), Email(), Length(max=120)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=80)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
@@ -24,3 +24,9 @@ class LoginForm(FlaskForm):
     username_or_email = StringField('Username/Email', validators=[DataRequired(), Length(max=120)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=80)])
     submit = SubmitField('Log In')
+
+    def validate(self, username_or_email, password):
+        user1 = User.query.filter_by(username=username_or_email.data, password=password.data).first()
+        user2 = User.query.filter_by(username=username_or_email.data, password=password.data).first()
+        if (not user1) and (not user2):
+            raise ValidationError('Login Failed. Please check username/email and password.')
