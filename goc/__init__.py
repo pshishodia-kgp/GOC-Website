1f9bd31856855c13e1f0a4979d46c2b9d16ae531
tsaraf20 <saraft20@gmail.com>
2020-12-31 17:35:20 +0530

    Adds time ago to posts and comments

--- goc/__init__.py
+++ goc/__init__.py
@@ -1,4 +1,4 @@
-import os
+import os, datetime, timeago
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -10,6 +10,10 @@ STATIC_DIR = os.path.join(".", "static")
 app = Flask(__name__, template_folder = TEMPLATE_DIR, static_folder = STATIC_DIR)
 
 app.config['SECRET_KEY'] = 'd035d622dc82b6465e417465da37a499'
+
+app.jinja_env.globals['timeago'] = timeago.format
+app.jinja_env.globals['datetime'] = datetime.datetime.utcnow()
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
